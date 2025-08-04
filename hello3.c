6bff2a2d5209b7a70eb0e73fce8a4a9dedabf7aa
Edward Morawski <edmorawski127@gmail.com>
2025-08-04 14:36:09 -0400

    Hello3 to explore program parm management

--- hello3.c
+++ hello3.c
@@ -1,3 +1,9 @@
+/*  hello3.c
+    This program, corresponding to chapter 10 in the book, does file 
+    manipulation. Added fgeatures will be entering files names
+    through program parameters.
+*/
+
 #include <stdio.h>
 #include <stdlib.h>
 
