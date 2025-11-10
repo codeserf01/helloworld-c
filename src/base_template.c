d5651151d83bac9471a452a28f81b057a25fae9a
Edward Morawski <edmorawski127@gmail.com>
2025-11-10 16:58:26 -0500

    Weekly update for backup

--- src/base_template.c
+++ src/base_template.c
@@ -22,8 +22,21 @@
 
 /* Buffers and vars that may be referenced throughout this listing                */
 /* Some basic input and control buffers, vars, etc. for this program.             */
+/* Default values - in case the program is run without input partms or config values */
+char def_cfg_filename    [] = "./template_config.txt\0"; // Default pgm configuration input file name
+char def_log_filename    [] = "./logfile.txt\0";         // Default output log file name
+char def_det_filename    [] = "./detail_log.txt\0";      // Default detailed log output file name
+char def_output_filename [] = "./output.txt\0";          // Default program standard processing output file name
 
-char pgm_name[40];                            // Program name - This program
+/* Set at runtime, either withthe default values (above) or runtime input parms */
+char pgm_name[40];                                    // Program name - This program, minus leading non-alpha chars
+char cfg_input          [100];                        // Program configuration file name
+char log_file           [100];                        // Program log output file
+char log_detail         [100];                        // Program detailed log output
+char output_file        [100];                        // Program standard output
+
+
+
 /*
 char todaydate[7];                            // System date
 char input_file_name[100];                    // Principle input file to this program
@@ -63,7 +76,7 @@ char input_file_findtype_u[100];              // FIND file type - in UPPER CASE
 int Get_runtime_parms(int, char *[]);
 
 
-
+/*------------------------------------------------------------------------------------------------------------------------------------*/
 int main(int argc, char *argv[])
 {
   int i   = 0;                                             // Generic increment counter
@@ -93,22 +106,42 @@ int main(int argc, char *argv[])
     i++;
   strcpy(pgm_name, argv[0]+i);                             // Keep the resulting program name
     
-  printf("\n Program: %s\n", pgm_name);                    // Display this program's name
-  printf(" Program Compile Date/Time: %s/%s\n\n", __DATE__, __TIME__);  // Display this program's compile date and time
+  printf("\nProgram: %s\n", pgm_name);                    // Display this program's name
+  printf("Program Compile Date/Time: %s/%s\n\n", __DATE__, __TIME__);  // Display this program's compile date and time
 
-  printf(" Current time: (local): %s, ", loc_Time_str);    // Print out the 'local time' string
-  printf(" (UTC): %s", UTC_Time_str);                      // include UTC time to the string 
+  printf("Current time: (local): %s, ", loc_Time_str);    // Print out the 'local time' string
+  printf("(UTC): %s", UTC_Time_str);                      // include UTC time to the string 
   printf("\n\n");                                          // finish line with line feed(s) 
+
+  /* Set default runtime values. These *may* be overridden with runtime program parms or input config file info */
+  // Note: pgm_name is already set (above)...
+
+  /* First initialize everything to NULL */
+  memset(cfg_input,   '\0', sizeof(cfg_input));    
+  memset(log_file,    '\0', sizeof(log_file));    
+  memset(log_detail,  '\0', sizeof(log_detail));    
+  memset(output_file, '\0', sizeof(output_file));    
+ 
+  /* Copy into the file names list the default values. These MAY be overridden with config or runtime parms */
+  strcpy(cfg_input,   def_cfg_filename);
+  strcpy(log_file,    def_log_filename);
+  strcpy(log_detail,  def_det_filename);
+  strcpy(output_file, def_output_filename);    
 
   /* Get any further run-time parameters */
   ret = 0;
   ret = Get_runtime_parms(argc, argv);
-  if (ret == 4)
+  if (ret == 1)                         // No runtime command line parameters included
   {
-    printf("*** Return code 4, ending this program run now.\n");
-
-    return ret;
+    printf("*** No runtime command line parameters. \n");
   }
+  else
+    if (ret == 4)                       // Problem with runtime command line parm(s) - ending program now.
+    {
+      printf("*** Invalid runtime command line parm - ending program run now.\n");
+
+      return ret;
+    }
 
 
   /* Now get the specific processing environment(s) vars that this program needs to work in,
@@ -117,7 +150,7 @@ int main(int argc, char *argv[])
      But, don't forget the immediate stuff such as system time and date, file names, etc....
   */
 
-  return 0;
+  return ret;
 }
 
 int Get_runtime_parms(int pgm_argc, char *pgm_argv[])
@@ -134,13 +167,13 @@ int Get_runtime_parms(int pgm_argc, char *pgm_argv[])
   /*
   First read in any run-time parameters and parse them.
   */
-    int param = 0;
+    int param = 1;            // .start with the 2nd parm - after the program name - '0'
 
-    printf("Program parameters (strings) at run time:\n");
+    // printf("Program parameters (strings) at run time:\n");
 
     // Simple alternative: display the first character of each parameter
-    printf("Input parms - first character of each parm at run time:\n");
-    printf("Parameter count: %d\n", pgm_argc);
+    // printf("Input parms - first character of each parm at run time:\n");
+    printf("Program command line parameter count: %d\n", pgm_argc);
     /*
       If too many parameters, then end an error message and end immediately 
     */
@@ -154,10 +187,10 @@ int Get_runtime_parms(int pgm_argc, char *pgm_argv[])
 
 
 
-    param = 0;
+    param = 1;
     while (param < pgm_argc)
     {
-      printf("Parameter %d is %c\n", param, *pgm_argv[param]);
+      printf("Parameter %d is %s\n", param, pgm_argv[param]);
       /* Parse the input parm */
 
 
@@ -174,6 +207,8 @@ int Get_runtime_parms(int pgm_argc, char *pgm_argv[])
       param++;
     }
 
-
-  return 0;
+  if (param > 1)
+    return 0;
+  else
+    return 1;
 }
